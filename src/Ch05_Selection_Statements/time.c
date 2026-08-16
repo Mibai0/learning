6bdf0aaa44a98497b4ee7e8fd2fac71d467f0327
Mibai <mibai_2007@outlook.com>
2026-08-16 23:05:59 +0800

    Add Time AMPM Display ON Time.c

--- src/Ch05_Selection_Statements/time.c
+++ src/Ch05_Selection_Statements/time.c
@@ -2,23 +2,25 @@
 
 int main(void){
   int h ,m;
+  char *timeAP; 
   printf("Enter A 24-Hour Time:");
 
   scanf("%d:%d" ,&h ,&m);
 
-  if (h > 24 || h < 0 || m >59 || m < 0) {
+  if (h > 23 || h < 0 || m >59 || m < 0) {
     printf("Wrong Time Input");
     return 1;
   }
 
   else if (h > 12){
     h = h - 12;
-  }
-  
-  else {
-    h = h;
+    timeAP = "PM";
   }
 
-  printf("Equivalent 12-hour time :%02d:%02d\n" ,h ,m);
+  else{
+    timeAP = "AM";
+  }
+
+  printf("Equivalent 12-hour time :%02d:%02d %s\n" ,h ,m ,timeAP);
   return 0;
 }
